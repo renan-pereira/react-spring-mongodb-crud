f1f0f14d693813d3008dd855cf4aac352b4ce1d9
renan-pereira <renangoespereira93@gmail.com>
2021-02-23 15:23:59 +0000

    alteracao servico 2

--- quarkus-getting-started/src/main/java/org/acme/GreetingResource.java
+++ quarkus-getting-started/src/main/java/org/acme/GreetingResource.java
@@ -32,7 +32,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/testePereira")
     public String testeRenan() {
-        return "Teste Marco deploy nova integracao";
+        return "Teste Marco 2 deploy nova integracao";
     }
 
 }
